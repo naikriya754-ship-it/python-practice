5eefa8b652ac9ff7f9ad5dd461fc2c27f4269846
Riya Naik <naikriya754@gmail.com>
2026-07-18 20:30:40 +0530

    Add files via upload

---
+++ two sum.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        prevmap = {} 
+        for i,n in enumerate(nums):
+            diff = target - n
+            if diff in prevmap:
+                return[prevmap[diff], i]
+            prevmap[n] = i
+        return
